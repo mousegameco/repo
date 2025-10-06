4f83f6c9a02b17d1e07f880fa644ea3a3bbde9fc
mousegameco <shaotongli4@gmail.com>
2025-10-06 22:34:08 +0800

    Update testground.py

--- testground.py
+++ testground.py
@@ -1,8 +1,11 @@
 
 input = [2, 3, 5, 4]
 output = []
+# x in range of 4
 for x in range(len(input)):
+    # y in range of 4 (differrent from x)
     for y in range(x + 1, len(input)):
+        #if two numbers added = 8 therefore their position were shown
         if int(input[x]) + int(input[y]) == 8:
             output.extend([x, y])
 
